65aded8a8caca0aed1e235dfe046b73f0c9f2931
Chris Pereira <chrispereira1@gmail.com>
2017-11-03 13:54:21 -0400

    add city search function

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -5,7 +5,11 @@ import { Http } from '@angular/http';
 export class Weather {
   constructor(private http: Http) {}
 
-  getWeather(lat, lon) {
+  getWeatherCity(city) {
+    return this.http.get('api.openweathermap.org/data/2.5/weather?q=' + city)
+  }
+
+  getWeatherLonLat(lat, lon) {
     return this.http.get('http://api.openweathermap.org/data/2.5/find?lat=' + lat +'&lon='+ lon +'&cnt=50')
   }
 }
